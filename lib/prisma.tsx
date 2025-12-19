d46c62519bca3ac92d2bac50f29efeb0c09e439f
jchukkarin <jchukkarin@gmail.com>
2025-12-19 17:32:08 +0700

    New ci/cd

--- lib/prisma.tsx
+++ lib/prisma.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/runtime/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -6,9 +6,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    accelerateUrl: process.env.PRISMA_ACCELERATE_URL,
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
