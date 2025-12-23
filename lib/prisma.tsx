c78a4ba35377cc8a75b5934eb220ef96a6deb76d
jchukkarin <jchukkarin@gmail.com>
2025-12-23 15:30:17 +0700

    CI

--- lib/prisma.tsx
+++ lib/prisma.tsx
@@ -6,7 +6,9 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient()
+  new PrismaClient({
+    log: ['warn', 'error'],
+  })
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
